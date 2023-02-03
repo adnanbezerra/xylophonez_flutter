a91125ee67c766e0a3ee7bb186a80d17ffeb68fc
seu nome <adnanbezerra@gmail.com>
2023-02-02 21:52:28 -0300

    refactor: add DRY principle

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,19 @@ class XylophoneApp extends StatelessWidget {
       ..dispose();
   }
 
+  Expanded buildButton(Color color, int buttonNumber) {
+    return Expanded(
+      child: TextButton(
+        onPressed: () {
+          playSound(buttonNumber);
+        },
+        child: Container(
+          color: color,
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -21,76 +34,13 @@ class XylophoneApp extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              Expanded(
-                child: TextButton(
-                  onPressed: () {
-                    playSound(1);
-                  },
-                  child: Container(
-                    color: Colors.red,
-                  ),
-                ),
-              ),
-              Expanded(
-                child: TextButton(
-                  onPressed: () {
-                    playSound(2);
-                  },
-                  child: Container(
-                    color: Colors.orange,
-                  ),
-                ),
-              ),
-              Expanded(
-                child: TextButton(
-                  onPressed: () {
-                    playSound(3);
-                  },
-                  child: Container(
-                    color: Colors.yellow,
-                  ),
-                ),
-              ),
-              Expanded(
-                child: TextButton(
-                  onPressed: () {
-                    playSound(4);
-                  },
-                  child: Container(
-                    color: Colors.greenAccent,
-                  ),
-                ),
-              ),
-              Expanded(
-                child: TextButton(
-                  onPressed: () {
-                    playSound(5);
-                  },
-                  child: Container(
-                    color: Colors.green,
-                  ),
-                ),
-              ),
-              Expanded(
-                child: TextButton(
-                  onPressed: () {
-                    playSound(6);
-                  },
-                  child: Container(
-                    color: Colors.lightBlue,
-                  ),
-                ),
-              ),
-              Expanded(
-                child: TextButton(
-                  onPressed: () {
-                    playSound(7);
-                  },
-                  child: Container(
-                    color: Colors.blue,
-                  ),
-                ),
-              ),
+              buildButton(Colors.red, 1),
+              buildButton(Colors.orange, 2),
+              buildButton(Colors.yellow, 3),
+              buildButton(Colors.greenAccent, 4),
+              buildButton(Colors.green, 5),
+              buildButton(Colors.blue, 6),
+              buildButton(Colors.blue.shade300, 7),
             ],
           ),
         ),
